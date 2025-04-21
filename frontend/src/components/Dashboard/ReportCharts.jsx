dc745fb25e9d220e2d8d81b222cc7c10031b603a
Eugene Hxz <eugenehh2016@gmail.com>
2025-04-21 21:13:47 +0800

    updated memo import

--- frontend/src/components/Dashboard/ReportCharts.jsx
+++ frontend/src/components/Dashboard/ReportCharts.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import Chart from "react-apexcharts";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
